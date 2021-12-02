3ed03d462cb2be10a257a45974a163227b37da91
ChandimaHerath <chandimaherath@hotmail.com>
2021-12-03 00:32:00 +0530

    DB schema added

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,20 @@ const dbURL = 'mongodb+srv://chandima:chandima123@cluster0.gtenz.mongodb.net/tod
 const app = express();
 app.use(express.json());
 
+//connecting database
 mongoose.connect(dbURL)
 .then(()=>console.log('connected to the database'))
 .catch((err)=>console.log(err))
 
+//DB Schema
+const listSchema = new mongoose.Schema({
+    title: String,
+    isActive: Boolean,
+    expireDate: Date
+  
+  });
+
+const List =  mongoose.model('List', listSchema);  
 
 app.listen(3000, ():void =>{
     console.log('Listing on server..!');
