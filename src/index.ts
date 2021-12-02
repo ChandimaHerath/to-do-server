c38b77dc69c85a3a6abb5039bd399020ca472aed
ChandimaHerath <chandimaherath@hotmail.com>
2021-12-03 00:23:05 +0530

    database connection implemented

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import express  from 'express';
+import mongoose from 'mongoose';
+
+const dbURL = 'mongodb+srv://chandima:chandima123@cluster0.gtenz.mongodb.net/todoDB?retryWrites=true&w=majority'
 
 const app = express();
+app.use(express.json());
+
+mongoose.connect(dbURL)
+.then(()=>console.log('connected to the database'))
+.catch((err)=>console.log(err))
 
 
 app.listen(3000, ():void =>{
